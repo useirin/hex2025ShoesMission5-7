8c1c600418f58f6bcc79047d4fe1d4878ff8d070
useirin <azuretoan@gmail.com>
2025-08-24 16:10:03 +0800

    新增：AOS 動態效果

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-import './assets/scss/all.scss';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
 
-console.log('Hello world');
+console.log("Hello world");
+
+// AOS 初始化
+AOS.init();
